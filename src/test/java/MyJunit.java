e9495390057e639c36eaa86e2c6e843b74882c3b
etu296 <etumahmuda@gmail.com>
2022-12-22 17:12:04 +0600

    test login

--- src/test/java/MyJunit.java
+++ src/test/java/MyJunit.java
@@ -5,7 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -20,8 +22,8 @@ public class MyJunit {
         driver.get("https://monarchmart.com/");
 
     }
-@Test
-    public void getTitle()
+
+    public void a_getTitle()
     {
 
         String titleActual = driver.getTitle();
@@ -30,15 +32,33 @@ public class MyJunit {
         Assert.assertEquals(titleExpected, titleActual);
 
     }
-    @Test
-    public void ifElementExist()
+
+    public void b_ifElementExist()
     {
       boolean status = driver.findElement(By.xpath("//body/div[@id='__next']/div[1]/div[1]/div[1]/div[1]/a[1]/span[1]/img[1]")).isDisplayed();
        Assert.assertTrue(status);
     }
-    @After
-    public void closeBrowser()
-    {
-        driver.close();
+
+    public void c_clickOnButton(){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+        driver.findElement(By.xpath("//header/div[1]/div[1]/div[1]/div[3]/div[4]/button[1]/div[1]")).click();
     }
+    @Test
+    public void d_loginUser()
+    {
+
+        driver.findElement(By.xpath("//header/div[1]/div[1]/div[1]/div[3]/div[4]/button[1]/div[1]")).click();
+        WebElement txtUserNam = driver.findElement(By.xpath("//body/div[@id='__next']/div[1]/main[1]/div[1]/div[1]/div[1]/form[1]/div[1]/input[1]"));
+        txtUserNam.sendKeys("01315655618");
+        WebElement txtPass = driver.findElement(By.xpath("//body/div[@id='__next']/div[1]/main[1]/div[1]/div[1]/div[1]/form[1]/div[2]/input[1]"));
+        txtPass.sendKeys("123456");
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+        driver.findElement(By.xpath("//body/div[@id='__next']/div[1]/main[1]/div[1]/div[1]/div[1]/form[1]/button[1]")).click();
+
+    }
+//    @After
+//    public void closeBrowser()
+//    {
+//        driver.close();
+//    }
 }
